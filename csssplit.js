326b1b26f1557e2d5a043799d4d20f0ca2bc469b
neo <neoruiz@yahoo.com>
2013-09-20 13:38:32 -0400

    Added bless parser

---
+++ csssplit.js
@@ -0,0 +1,173 @@
+var path = require('path'),
+	fs = require('fs'),
+	util = require('util'),
+	console = require('console'),
+	bless = require('bless'),
+	options = {
+	    cacheBuster: true,
+	    cleanup: true,
+	    compress: false,
+	    force: true,
+	    imports: true
+	},
+	input = '../css/advertiserUi.css',
+	output = input;
+
+
+function noun(noun, variable) {
+    if (variable != 1) {
+        noun += 's';
+    }
+    return noun;
+}
+
+function formatNumber (nStr) {
+    nStr += '';
+    var x = nStr.split('.');
+    var x1 = x[0];
+    var x2 = x.length > 1 ? '.' + x[1] : '';
+
+    var rgx = /(\d+)(\d{3})/;
+    while (rgx.test(x1)) {
+        x1 = x1.replace(rgx, '$1' + ',' + '$2');
+    }
+
+    return x1 + x2;
+}
+
+var parseCss = function (e, data) {
+    if (e) {
+    	if (e.errno === 34) {
+    		console.log('AMP SPLIT CSS: [Error - file may be missing] Msg:' + e.message);	
+    	} else {
+    		console.log('AMP SPLIT CSS: ' + e.message);	
+    	}
+        process.exit(1);
+    }
+
+new (bless.Parser)({
+        output: output,
+        options: options
+    }).parse(data, function (err, files, numSelectors) {
+        if (err) {
+            throw err;
+            process.exit(1);
+        } else {
+            try {
+                var selectorNoun = noun('selector', numSelectors);
+                numSelectors = formatNumber(numSelectors);
+                var message = 'Source CSS file contained ' + numSelectors + ' ' + selectorNoun + '.',
+                    numFiles = files.length,
+                    fileNoun = noun('file', numFiles);
+
+                if(numFiles > 1 || input != output) {
+                    for (var i in files) {
+                        var file = files[i],
+                            fd = fs.openSync(file.filename, 'w');
+                        fs.writeSync(fd, file.content, 0, 'utf8');
+                    }
+
+                    message += ' ' + numFiles + ' CSS ' + fileNoun + ' created.';
+                } else {
+                    message += ' No changes made.';
+                }
+
+
+                bless.Parser.cleanup(options, output, function(err, files) {
+                    if (err) {
+                        throw err;
+                        process.exit(1);
+                    } else {
+                        var oldVerb;
+
+                        for (var i in files) {
+                            var file = files[i];
+
+                            if(! options.cleanup) {
+                                oldVerb = 'renamed';
+                                var ext = path.extname(file),
+                                    dest = file.replace(ext, '-old' + ext),
+                                    read = fs.createReadStream(file),
+                                    write = fs.createWriteStream(dest);
+
+                                read.on('end', function (err) {
+                                    if (err) {
+                                        throw err;
+                                        process.exit(1);
+                                    }
+                                });
+
+                                util.pump(read, write);
+                            } else {
+                                oldVerb = 'removed';
+                            }
+
+                            fs.unlink(files[i], function (err) {
+                                if (err) {
+                                    throw err;
+                                    process.exit(1);
+                                }
+                            });
+                        }
+
+                        var numOld = files.length;
+
+                        if (numOld > 0) {
+                            var removedFileNoun = noun('file', numOld);
+                            message += ' Additional CSS ' + removedFileNoun + ' no longer needed. ' + numOld + ' additional ' + removedFileNoun + ' ' + oldVerb + '.';
+                        }
+
+                        console.log('AMP CSS SPLIT: ' + message);
+                    }
+                });
+
+            } catch (e) {
+                throw e;
+                process.exit(2);
+            }
+        }
+    });
+};
+
+if (input != '-') {
+    fs.readFile(input, 'utf-8', parseCss);
+} else {
+    process.stdin.resume();
+    process.stdin.setEncoding('utf8');
+
+    var buffer = '';
+    process.stdin.on('data', function(data) {
+        buffer += data;
+    });
+
+    process.stdin.on('end', function() {
+        parseCss(false, buffer);
+    });
+}
+
+/*
+fs.watch('../css/', function(e, fn){
+	
+	if (e === "change" && fn.toLowerCase() === "advertiserui.css"){
+		console.log(e, fn);
+		if (input != '-') {
+		    fs.readFile(input, 'utf-8', parseCss);
+		} else {
+		    process.stdin.resume();
+		    process.stdin.setEncoding('utf8');
+
+		    var buffer = '';
+		    process.stdin.on('data', function(data) {
+		        buffer += data;
+		    });
+
+		    process.stdin.on('end', function() {
+		        parseCss(false, buffer);
+		    });
+		}
+	}
+});
+*/
+
+
+
